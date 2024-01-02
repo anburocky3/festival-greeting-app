c77dd998624dcbdff4ea9d8d9148a6206249d748
Anbuselvan Rocky <15264938+anburocky3@users.noreply.github.com>
2024-01-02 13:54:49 +0530

    feat: Displays greeting card based on dynamic name input

--- assets/js/greeting.js
+++ assets/js/greeting.js
@@ -1,9 +1,19 @@
 import Fireworks from "fireworks-js";
 
 const fwContainer = document.querySelector("#fireworks");
+const greeterNameEl = document.querySelector("#greeterName");
 
 const fireworks = new Fireworks(fwContainer);
 
 fireworks.start();
 // fireworks.stop()
-console.log("Working!");
+
+const url = new URL(window.location.href);
+const name = url.searchParams.get("name");
+
+if (name) {
+  // Display that value on UI.
+  greeterNameEl.textContent = name;
+} else {
+  location.href = "/festival-greeting-app/index.html";
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,10 @@ const form = document.querySelector("form#greeterForm");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  console.dir(e.target[0]);
+  const greeterName = e.target[0].value;
 
-  window.location.href = "/festival-greeting-app/greeting.html";
+  window.location.href =
+    "/festival-greeting-app/greeting.html?name=" + greeterName;
   // TODO: Form-validations on v2.
   //   if(form)
 
